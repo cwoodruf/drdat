0f49251fb9ef06624763e9e5f7b172371bf89b74
cwoodruf <cwoodruf@f6cee6d0-0ffb-11df-9cc9-4389dd015ce2>
2010-02-11 07:52:24 +0000

    added some comments to index.php

--- trunk/smi/index.php
+++ trunk/smi/index.php
@@ -3,3 +3,9 @@
 # if __SMI__ is not false then its not defined - php defines unknown strings to true (ps: gah!)
 define('__SMI__',false);
 require_once('lib/includes.php');
+# first check user input
+# checking user input should dispatch some work to: 
+# the model - manipulate some data ... maybe 
+# and then the view - show some response to the end user
+# most likely the controller will call the other code and
+# all we do here is call a controller function or method
