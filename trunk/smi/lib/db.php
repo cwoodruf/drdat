b56dcec54c42c4e9c0c42f04969c073cd6a9c827
cwoodruf <cwoodruf@f6cee6d0-0ffb-11df-9cc9-4389dd015ce2>
2010-02-11 02:59:13 +0000

    added basic smi code - not tested

---
+++ trunk/smi/lib/db.php
@@ -0,0 +1,5 @@
+<?php
+# database access - don't put actual logins, passwords in here
+if (!defined(__SMI__)) die("no direct access.");
+# see example file for this
+require("lib/.localsettings.php");
