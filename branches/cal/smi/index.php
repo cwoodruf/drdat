d77644abfc6d04b142ba5e95d576d2f2b6d7d494
cwoodruf <cwoodruf@f6cee6d0-0ffb-11df-9cc9-4389dd015ce2>
2010-02-11 07:41:59 +0000

    changed index.php title to distinguish this version of smi from the main one

--- branches/cal/smi/index.php
+++ branches/cal/smi/index.php
@@ -1,4 +1,4 @@
-<h3>Study management portal</h3>
+<h3>Study management portal - test version</h3>
 <?php
 # if __SMI__ is not false then its not defined - php defines unknown strings to true (ps: gah!)
 define('__SMI__',false);
