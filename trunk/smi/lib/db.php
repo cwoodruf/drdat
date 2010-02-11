a1b7464990ae0aabb13af5bf175b88c95c4f60d5
cwoodruf <cwoodruf@f6cee6d0-0ffb-11df-9cc9-4389dd015ce2>
2010-02-11 03:05:18 +0000

    changed where .localsettings.php gets loaded

--- trunk/smi/lib/db.php
+++ trunk/smi/lib/db.php
@@ -1,5 +1,3 @@
 <?php
 # database access - don't put actual logins, passwords in here
 if (!defined(__SMI__)) die("no direct access.");
-# see example file for this
-require("lib/.localsettings.php");
