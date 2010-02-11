a0a1568c43e2454c56d4b7047aa7ccd6a8c16354
cwoodruf <cwoodruf@f6cee6d0-0ffb-11df-9cc9-4389dd015ce2>
2010-02-11 02:59:13 +0000

    added basic smi code - not tested

---
+++ smi/lib/example.localsettings.php
@@ -0,0 +1,6 @@
+<?php
+# use the .localsettings.php file to hold settings that should not be defined in subversion
+define('DRDAT_DBHOST','localhost');
+define('DRDAT_DB','drdat');
+define('DRDAT_DBLOGIN','drdat');
+define('DRDAT_DBPW','');
---
+++ smi/lib/includes.php
@@ -0,0 +1,5 @@
+<?php
+if (!defined('__SMI__')) die('no direct access');
+require_once('lib/model.php');
+require_once('lib/controller.php');
+require_once('lib/view.php');
