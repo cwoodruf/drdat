fd16640134bf0f7169b7f5f9aad695a0bc3c6782
cwoodruf <cwoodruf@f6cee6d0-0ffb-11df-9cc9-4389dd015ce2>
2010-02-13 05:00:27 +0000

    starting to add db related code

--- branches/cal/smi/lib/db.php
+++ branches/cal/smi/lib/db.php
@@ -2,3 +2,15 @@
 # database access - don't put actual logins, passwords in here
 if (__SMI__) die("no direct access.");
 require('lib/drdat-schema.php');
+class DRDAT {
+	private $schema;
+	private $conn;
+
+	function connect() {
+		$this->conn = mysql_connect(DRDAT_DBHOST, DRDAT_DBLOGIN, DRDAT_DBPW);
+		if (!$this->conn) die("can't connect: ".mysql_error());
+		$res = mysql_select_db(DRDAT_DB, $this->conn);
+		if (!$res) die("can't select database: ".mysql_error());
+		return $this->conn;
+	}
+}
