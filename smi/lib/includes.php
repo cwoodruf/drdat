dc97ecef99766412d05abe4ad9f1a44c977a9708
cwoodruf <cwoodruf@f6cee6d0-0ffb-11df-9cc9-4389dd015ce2>
2010-02-14 05:10:47 +0000

    moved where .localsettings.php was as it does not work in other location

--- smi/lib/includes.php
+++ smi/lib/includes.php
@@ -1,5 +1,8 @@
 <?php
 if (__SMI__) die("no direct access.");
+# the following file is not stored in subversion for security reasons
+# see lib/example.localsettings.php
+@include_once('lib/.localsettings.php');
 # db access
 # does stuff to data, doesn't necessarily automate anything 
 require_once('lib/model.php');
@@ -8,9 +11,3 @@ require_once('lib/model.php');
 require_once('lib/controller.php');
 # show something
 require_once('lib/view.php');
-# the following file is not stored in subversion for security reasons
-# see lib/example.localsettings.php
-# place this here if you want to override settings made in other libraries
-# generally you should not be executing code in the above libraries but rather
-# defining functions or classes
-@include_once('lib/.localsettings.php');
