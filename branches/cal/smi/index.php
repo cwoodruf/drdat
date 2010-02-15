0aae46866d14c6f22d7072a40e14314502851311
cwoodruf <cwoodruf@f6cee6d0-0ffb-11df-9cc9-4389dd015ce2>
2010-02-15 17:20:43 +0000

    signing up and logging in now working

--- branches/cal/smi/index.php
+++ branches/cal/smi/index.php
@@ -6,9 +6,15 @@ define('__SMI__',false);
 session_start();
 require_once('lib/includes.php');
 
+$action = $_REQUEST['action'];
+
 $user = new Login;
-if (!$user->valid()) {
+if (!$unblocked[$action] and !$user->valid()) {
 	$smarty->display('login.tpl');
 	exit();
 }
+
+$do = new Doit;
+$template = $do->process($action);
+$smarty->display($template);
 
