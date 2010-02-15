126e67e408a9ebee41d5306531072a1759780445
cwoodruf <cwoodruf@f6cee6d0-0ffb-11df-9cc9-4389dd015ce2>
2010-02-15 16:21:52 +0000

    fleshed out controller part to do with logins

--- branches/cal/smi/index.php
+++ branches/cal/smi/index.php
@@ -2,4 +2,13 @@
 <?php
 # if __SMI__ is not false then its not defined - php defines unknown strings to true (ps: gah!)
 define('__SMI__',false);
+
+session_start();
 require_once('lib/includes.php');
+
+$user = new Login;
+if (!$user->valid()) {
+	$smarty->display('login.tpl');
+	exit();
+}
+
