aa64738dbd2eb96df8e9cb7f3dbd86b2c4a61edb
cwoodruf <cwoodruf@f6cee6d0-0ffb-11df-9cc9-4389dd015ce2>
2010-02-17 01:27:26 +0000

    removed templates_c

--- branches/cal/smi/view/templates_c/%%0E^0E4^0E407559%%footer.tpl.php
+++
@@ -1,4 +0,0 @@
-<?php /* Smarty version 2.6.26, created on 2010-02-15 19:27:42
-         compiled from footer.tpl */ ?>
-</body>
-</html>
--- branches/cal/smi/view/templates_c/%%6D^6D7^6D7C5625%%test.tpl.php
+++
@@ -1,6 +0,0 @@
-<?php /* Smarty version 2.6.26, created on 2010-02-14 23:24:10
-         compiled from test.tpl */ ?>
-<?php require_once(SMARTY_CORE_DIR . 'core.load_plugins.php');
-smarty_core_load_plugins(array('plugins' => array(array('function', 'test', 'test.tpl', 2, false),)), $this); ?>
-<h4>did this work?</h4>
-<?php echo smarty_function_test(array(), $this);?>
