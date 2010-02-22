1bb55b846673548681ed3d1eba87ff50b57268cb
cwoodruf <cwoodruf@f6cee6d0-0ffb-11df-9cc9-4389dd015ce2>
2010-02-22 12:55:19 +0000

    minor formatting change to index.php

--- branches/cal/smi/index.php
+++ branches/cal/smi/index.php
@@ -6,6 +6,7 @@ Licensed under the Perl Artistic License version 2.0
 http://www.perlfoundation.org/attachment/legal/artistic-2_0.txt
 ---------------------------------------------------------------
 */
+
 /**
  * main entry point for all request processing
  */
