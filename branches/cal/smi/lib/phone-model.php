0c590d0b298009b712bf4f82fad88aaa3416d0c6
cwoodruf <cwoodruf@f6cee6d0-0ffb-11df-9cc9-4389dd015ce2>
2010-04-05 13:48:03 +0000

    minor fix to phone

--- branches/cal/smi/lib/phone-model.php
+++ branches/cal/smi/lib/phone-model.php
@@ -22,22 +22,63 @@ class PhoneSchedule extends Schedule {
 }
 
 class PhoneTask extends Task {
-	
+	private function datafy($task_id,$study_id) {
+		$s = new Schedule;
+		$this->sched = $s->getone(array('task_id' => $task_id, 'study_id' => $study_id));
+		if (!count($this->sched)) 
+			throw new Exception("No schedule!");
+		$this->parseforms($task_id);
+		if (!is_array($this->forms)) 
+			throw new Exception("No forms!");
+	}
 	public function forms2xml($task_id,$study_id) { //make MORE CHECKS & throw exceptions this is for tasks
 		try {
-			$s = new Schedule;
-			$sched = $s->getone(array('task_id' => $task_id, 'study_id' => $study_id));
-			if (!count($sched)) 
-				throw new Exception("No schedule!");
-			$this->parseforms($task_id);
-			if (!is_array($this->forms)) 
-				throw new Exception("No forms!");
-			return $this->formstring2xml($sched);
-
+			$this->datafy($task_id,$study_id);
+			return $this->formstring2xml();
 		} catch (Exception $e) {
 			return "ERROR : {$e->getMessage()}";
 		}
-	
 	}
-	
+
+	public function forms2html($task_id,$study_id) {
+		try {
+			$this->parseforms($task_id,'html');
+			if (!is_array($this->forms)) 
+				throw new Exception("No forms!");
+			return $this->formstring2html();
+		} catch (Exception $e) {
+			return "ERROR : {$e->getMessage()}";
+		}
+	}
+	public function formstring2html() {
+		
+		$html = <<<HTML
+    <h3 class="task_title">{$this->task['task_title']}</h3>
+HTML;
+		$num = 0;
+		if (is_array($this->forms)) {
+			foreach ($this->forms as $form) {
+				$html .= <<<HTML
+    <div id="form$id" class="form">
+
+HTML;
+				foreach ($form as $idata) {
+					$num++;
+					$instruction = trim($idata['instruction']);
+					$html .= <<<HTML
+        <div class="taskitem">
+            <input type="hidden" name="taskitem_id[]" value="$num">
+            <input type="hidden" name="instruction[]" value="$instruction">
+            <h4 class="instruction">$num $instruction</h4>
+            <div class="format">{$idata['format']}</div>
+        </div>
+
+HTML;
+				}
+				$html .= <<<HTML
+    </div>
+HTML;
+			}
+		}
+	}
 }
--- branches/cal/smi/phone.php
+++ branches/cal/smi/phone.php
@@ -12,4 +12,3 @@ require_once('lib/phone-controller.php');
 
 $action = new PhoneAction;
 print $action->process();
-
