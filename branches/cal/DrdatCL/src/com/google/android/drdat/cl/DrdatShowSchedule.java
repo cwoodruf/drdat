41eb34c73917cc5aa52ddb4676a29fd98b91d52c
cwoodruf <cwoodruf@f6cee6d0-0ffb-11df-9cc9-4389dd015ce2>
2010-04-22 09:52:37 +0000

    started work on update and schedule display utilities for cl

---
+++ branches/cal/DrdatCL/src/com/google/android/drdat/cl/DrdatShowSchedule.java
@@ -0,0 +1,16 @@
+package com.google.android.drdat.cl;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DrdatShowSchedule extends Activity {
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	
+	    // TODO Auto-generated method stub
+	}
+
+}
