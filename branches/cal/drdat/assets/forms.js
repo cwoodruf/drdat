e42c28db013291d70d9e602ab03d2f1595c73b9b
cwoodruf <cwoodruf@f6cee6d0-0ffb-11df-9cc9-4389dd015ce2>
2010-05-12 08:31:41 +0000

    fixed two problems with form generation with custom html forms: radios now work, default values now work; updated the help file

--- branches/cal/drdat/assets/forms.js
+++ branches/cal/drdat/assets/forms.js
@@ -8,9 +8,16 @@ function fill(form) {
 	for (i=0; i<form.elements.length; i++) {
 		var t = form.elements[i].type; 
 		name = window.encodeURIComponent(form.elements[i].name);
+		if (!DrdatForms.exists(name)) return true;
 		value = DrdatForms.getField(name); 
 		if (t == 'checkbox' && value != '') {
 			form.elements[i].checked = true;
+	    } else if (t == 'radio') {
+	        if (value == form.elements[i].value) {
+                form.elements[i].checked = true;
+            } else {
+                form.elements[i].checked = false;
+            }                
 		} else if (t == 'text' || t.match(/select.*/)) { 
 			form.elements[i].value = window.decodeURIComponent(value);
 		}
@@ -20,9 +27,10 @@ function fill(form) {
 
 function save(form) {
 	for (i=0; i<form.elements.length; i++) {
+	    var t = form.elements[i].type;
 		if (form.elements[i].name == 'action') continue;
-		if (form.elements[i].type == 'submit') continue;
-		if (form.elements[i].type == 'checkbox') {
+		if (t == 'submit') continue;
+		if (t == 'checkbox' || t == 'radio') {
 			if (form.elements[i].checked) val = form.elements[i].value;
 			else continue;
 		} else {
